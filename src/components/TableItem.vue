73e67d4c7237c95b033a0759ac8287d6dcbb2a80
Al-Plb <49525684+Al-Plb@users.noreply.github.com>
2019-09-27 13:49:41 +0200

    table change

--- src/components/TableItem.vue
+++ src/components/TableItem.vue
@@ -9,17 +9,25 @@
         @request="onRequest"
         selection="none"
         v-if='!showCard'
+        :filter="filter"
       >
+      
+        <template v-slot:top-right>
+          <q-input dense debounce="300" color="primary" v-model="filter">
+            
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+
         <template v-slot:body="props" >
             <q-tr 
             :props="props" 
             class="cursor-pointer" 
-            @click.native="selectRow(props.row.__index)"
+            @click.native="selectRow(props.row)"
             >
-                <q-td key="name" :props="props">{{ props.row.name }}</q-td> 
-                <q-td key="client" :props="props">{{ props.row.client }}</q-td> 
-                <q-td key="family" :props="props">{{ props.row.family }}</q-td> 
-                <q-td key="type" :props="props">{{ props.row.type }}</q-td> 
+                <q-td v-for="item in colmns" :key="item" :props="props">{{ props.row[item] }}</q-td> 
             </q-tr>
         </template> 
       </q-table>
@@ -30,17 +38,8 @@
             <q-card class="my-card" v-if='showCard'>
                 <q-btn class="float-right" @click="switchShow">Closed</q-btn>
                 <div class="text-h6 q-pa-md">Infos element</div>
-                <q-card-section>
-                    Name: {{datas[idSelected].name}}
-                </q-card-section>
-                <q-card-section>
-                    Client: {{datas[idSelected].client}}
-                </q-card-section>
-                <q-card-section>
-                    Family: {{datas[idSelected].family}}
-                </q-card-section>
-                <q-card-section>
-                    Type: {{datas[idSelected].type}}
+                <q-card-section v-for="item in colmns">
+                    {{item}}: {{selectedItem[item]}}
                 </q-card-section>
             </q-card>
         </div>
@@ -48,16 +47,20 @@
     </div>    
 </template>
 
-
-
 <script>
 export default {
   name:'ApiResult',
   data: function() {
     return {
+        filter: "",
         showCard: false,
-        idSelected: 0,
-        lorem: "lorem lorem lorem lorem lorem lorem lorem",
+        selectedItem: {},
+        colmns: [
+          "name",
+          "type",
+          "client",
+          "family"
+        ],
         pagination: {               
             page: 1,        
             rowsPerPage: 5,        
@@ -70,18 +73,22 @@ export default {
             label: 'name',
             align: 'left',
             field: row => row.name,
-            format: val => `${val}`,
             sortable: true
             },
             { name: 'client', align: 'center', label: 'client', field: 'client', sortable: true },
             { name: 'family', label: 'family', field: 'family', sortable: true },
-            { name: 'type', label: 'type', field: 'type' },
+            { name: 'type', label: 'type', field: 'type', sortable: true },
         ],
         datas: []
     }
     
   },
- computed: {
+  watch: {
+    filtered: function() {
+      this.filter
+    }
+  },
+  computed: {
     offset() {      
       return this.pagination.rowsPerPage * (this.pagination.page - 1);    
     },    
@@ -94,30 +101,34 @@ export default {
   }, 
   methods: {
     fetchData() {
-          this.axios.get("https://api.cmdb-uat.corp.org.ebrc.local/items?limit=" + this.limit + "&offset=" + this.offset)
+           this.axios.get("https://api.cmdb-uat.corp.org.ebrc.local/items?limit=" + 
+           this.limit + "&offset=" + this.offset+"&filter={\"info.name\":{\"$regex\":\""+ this.filter + "\",\"$options\":\"i\"}}")
           .then(response => {
-            console.log(response.data.data);
+            //console.log(response.data.data);
             this.datas = _.map(response.data.data, 'info');
             this.pagination.rowsNumber = response.data.total;
           });
     },
     onRequest(props) {
         this.pagination = props.pagination;
-        //console.log(this.limit)
-        //console.log(this.offset)
         this.fetchData();
     },
     selectRow(value) { 
-        console.log(value)
-        this.idSelected = value;
+        //console.log(value)
+        this.selectedItem = value;
         this.switchShow();
-        //console.log(this.showCard)
-        console.log(this.datas)
+        //console.log(this.datas)
     },
     switchShow() {
         this.showCard = !this.showCard
-    },
+    }
+
   }
 }
-
 </script>
+<style scope>
+.my-card {
+  width: 100%;
+  max-width: 500px;
+}
+</style>
