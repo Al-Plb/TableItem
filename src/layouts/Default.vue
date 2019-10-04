2412e762b7b019b4d1e0a99785ede28b7349b90c
Al-Plb <49525684+Al-Plb@users.noreply.github.com>
2019-10-04 18:01:02 +0200

    commit default

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -15,7 +15,26 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        
+          <div v-show="connected">
+           <i class="material-icons">
+            person</i> Logged as {{userName}} ({{userRole}})
+          <q-btn-dropdown label="">
+            <q-list>
+              <q-item clickable v-close-popup @click="viewProfil">
+                <q-item-section>
+                  <q-item-label>Infos profil</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="logOut">
+                <q-item-section>
+                  <q-item-label>Logout</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -50,6 +69,22 @@
             <q-item-label>Table</q-item-label>
           </q-item-section>
         </q-item>
+        <q-item to="/login" exact>
+          <q-item-section avatar>
+            <q-icon name="eva-info-outline" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Login</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/admin" exact v-if="showAdmin">
+          <q-item-section avatar>
+            <q-icon name="eva-info-outline" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Admin</q-item-label>
+          </q-item-section>
+        </q-item>
 
        
       </q-list>
@@ -63,12 +98,56 @@
 
 <script>
 export default {
-  name: 'LayoutDefault',
-
-  data () {
-    return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+    name: 'LayoutDefault',
+    data() {
+        return {
+          leftDrawerOpen: this.$q.platform.is.desktop,
+          showAdmin: false
+        }
+    },
+    computed: {
+        userName() {
+            return this.$store.state.loggedUser.name
+        },
+        userRole() {
+            return this.$store.state.loggedUser.role
+        },
+        connected() {
+            return this.$store.getters.connected;
+        },
+        roleUser(){
+            return this.$store.state.loggedUser.role;
+        },
+        selectedRole(){
+            return this.$store.state.users.find(element => element.name === this.selectedItem.name).role
+        }
+    
+    },
+    methods: {
+    viewProfil() {
+      this.$router.push('/profil')
+    },
+    logOut () {
+      let noUser = {name: "", role: ""};
+      this.$store.dispatch('change', noUser);
+      this.$router.push('/').catch(err => {});
+    },
+    updateRole(value){
+            let user={name: this.selectedItem.name, role: value}
+            this.$store.dispatch('updateUser', user);
+        
+    },
+    admin() {
+      if(roleUser === ('SuperAdmin && Admin')) {
+        this.showAdmin = !showAdmin;
+      }
     }
-  }
+   }
 }
 </script>
+
+<style>
+.material-icons {
+  font-size: 18px;
+}
+</style>
