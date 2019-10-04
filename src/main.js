ccfbe3fb71ab670d5156c6346995428775e1c325
Al-Plb <49525684+Al-Plb@users.noreply.github.com>
2019-10-04 17:59:44 +0200

    main commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './quasar'
-import router from './router'
+import {router} from './router'
+import store  from './store.js'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -9,11 +10,17 @@ import VueAxios from 'vue-axios'
 import _ from "lodash";
 Vue.prototype._ = _;
 
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+Vue.use(Chartkick.use(Chart))
+
 Vue.use(VueAxios, axios)
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
